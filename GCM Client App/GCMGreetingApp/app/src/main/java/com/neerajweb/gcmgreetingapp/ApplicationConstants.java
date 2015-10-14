c79257f40ffb9324a28174fdf94536b85a897269
ngoswami1978 <neeraj.login@gmail.com>
2015-10-14 10:59:23 +0530

    Update Version 1.0
    Update Version 1.0

---
+++ GCM Client App/GCMGreetingApp/app/src/main/java/com/neerajweb/gcmgreetingapp/ApplicationConstants.java
@@ -0,0 +1,17 @@
+package com.neerajweb.gcmgreetingapp;
+
+public interface ApplicationConstants {
+
+	// Php Application URL to store Reg ID created
+	//static final String APP_SERVER_URL = "http://192.168.2.4:9000/gcmwebapp/insertuser.php";
+
+	static final String APP_SERVER_URL = "http://www.myandroidng.com/Apartment/greeting/insertuser.php";
+
+	// Google Project Number
+	//static final String GOOGLE_PROJ_ID = "837715578074";
+
+	static final String GOOGLE_PROJ_ID = "677629339095";
+
+	static final String MSG_KEY = "m";
+
+}
